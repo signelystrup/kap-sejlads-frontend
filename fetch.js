1d4a8ad317d0efa23dbbdfdb3ad078a8b3a4ca6d
signelystrup <166133407+signelystrup@users.noreply.github.com>
2025-06-10 10:32:20 +0200

    generisk post metode virker

--- fetch.js
+++ fetch.js
@@ -4,22 +4,46 @@ export function getAll(endpoint){
     return fetch(url + endpoint)
         .then(response => {
             if (!response.ok){
-                console.log("error fetching: " + url + endpoint);
+                console.log("error fetching: GET " + url + endpoint);
             }
 
             return response.json();
         }).catch(error => console.error(error));
 }
 
-/*
 export function getById(endpoint, id){
     return getAll(endpoint + "/" + id);
 }
 
-const id = 1;
+export function add (endpoint, responseBody){
 
-getAll("/ware/all");
-getAll("/ware/" + id);
+    return fetch(url + endpoint, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(responseBody) //send entity to backend
 
- */
+        }).then(response => {
+            if (!response.ok){
+                console.log("error fetching: POST " + url + endpoint)
+            }
 
+            return response.json();
+
+    }).catch(error => console.error(error));
+}
+
+export function update (endpoint, responseBody){
+
+}
+
+export function deleteEntity (endpoint, id){
+
+}
+
+//testing:
+getById("/boat", 1).then(boat => { //get boat object.
+    delete boat.id; //remove id from boat object.
+    add("/boat", boat).then(boat => console.log(boat)); //post boat
+});
