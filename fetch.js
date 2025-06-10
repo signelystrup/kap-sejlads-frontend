3e572f99a9fb8e10689de96cfde2f14748b3eb4a
signelystrup <166133407+signelystrup@users.noreply.github.com>
2025-06-10 10:50:55 +0200

    PUT metode

--- fetch.js
+++ fetch.js
@@ -34,16 +34,39 @@ export function add (endpoint, responseBody){
     }).catch(error => console.error(error));
 }
 
-export function update (endpoint, responseBody){
+export function update (endpoint, id, responseBody){
+    return fetch(url + endpoint + "/" + id, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(responseBody) //send entity to backend
 
+    }).then(response => {
+        if (!response.ok){
+            console.log("error fetching: PUT " + url + endpoint)
+        }
+
+        return response.json();
+
+    }).catch(error => console.error(error));
 }
 
 export function deleteEntity (endpoint, id){
 
 }
 
-//testing:
+//test POST:
 getById("/boat", 1).then(boat => { //get boat object.
     delete boat.id; //remove id from boat object.
     add("/boat", boat).then(boat => console.log(boat)); //post boat
 });
+
+//test PUT:
+getById("/boat", 1).then(boat => {
+    boat.boatType = 2;
+    update("/boat", 42, boat).then(boat =>console.log(boat));
+})
+
+
+
